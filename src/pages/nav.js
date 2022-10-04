bc8a28ac1e243879b1244b5805c05ad3c40ae1bb
Lorenzo Tinsley <lorenzotinsley@gmail.com>
2022-10-03 23:12:46 -0400

    nav styling

--- src/pages/nav.js
+++ src/pages/nav.js
@@ -6,17 +6,16 @@ const Nav = (props) => {
 
     return (
         <>
-            <nav className="flex justify-between items-center p-5 bg-darkEletricBlue text-white font-alkalami px-2 sm:px-4 w-full z-20 top-0 left-0 ">
-                <div className="flex justify-between items-center cursor-pointer">
-                    <div>
-                        {/* Logo */}
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-1">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
-                        </svg>
+            <nav className="flex justify-between items-center p-3 bg-darkEletricBlue text-white font-alkalami px-2 sm:px-4 w-full z-20 top-0 left-0 ">
+                <div className="flex items-center cursor-pointer">
+                    {/* Logo */}
 
-                    </div>
-                    <div>
-                        <h1>TMS</h1>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-11 mr-1 fill-almond">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
+                    </svg>
+
+                    <div className="self-end">
+                        <h1 className="text-almond text-2xl">TMS</h1>
                     </div>
                 </div>
 
@@ -27,11 +26,11 @@ const Nav = (props) => {
                 </ul>
 
                 <div>
-                    <button className="hidden rounded p-2 md:block">Get a Quote Now!</button>
+                    <button className="hidden rounded p-2 md:block bg-cafe">Get a Quote Now!</button>
                 </div>
-                
+
                 {/* Mobile Icon */}
-                <div 
+                <div
                     className="flex cursor-pointer md:hidden"
                     onClick={() => setNavbar(!navBar)}
                 >
@@ -49,8 +48,8 @@ const Nav = (props) => {
                     <li className="mb-3 cursor-pointer text-lg">About</li>
                 </ul>
                 <div>
-                <button className="rounded p-2 text-lg">Get a Quote Now!</button>
-                </div>     
+                    <button className="rounded p-2 text-lg">Get a Quote Now!</button>
+                </div>
             </div>
         </>
     );
