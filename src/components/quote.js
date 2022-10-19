1cb558c4c54ed5a8005c88945db6c9385f4c88cf
Lorenzo Tinsley <lorenzotinsley@gmail.com>
2022-10-19 19:28:21 -0400

    Added inputs in Quote Form

--- src/components/quote.js
+++ src/components/quote.js
@@ -2,44 +2,48 @@ import { useState } from "react";
 
 
 const Quote = (props) => {
-   
+
 
     const [zipcode1, setZipcode1] = useState('');
     const [zipcode2, setZipcode2] = useState('');
     const [email, setEmail] = useState('');
+    const [bedrooms, setBedrooms] = useState(0);
+    const [homeType, setHometype] = useState('House')
 
-    function submitHandler(event){
+    function submitHandler(event) {
         event.preventDefault()
 
-         let formState = {
+        let formState = {
             zipcode1: zipcode1,
             zipcode2: zipcode2,
+            bedrooms: bedrooms,
+            homeType: homeType,
             email: email
-         }
+        }
 
-         console.log(formState)
-         try {
+        console.log(formState)
+        try {
             fetch(`https://api.zip-codes.com/ZipCodesAPI.svc/1.0/CalculateDistance/ByZip?fromzipcode=${zipcode1}&tozipcode=${zipcode2}&key=${process.env.REACT_APP_APIKEY}`)
                 .then(response => response.json())
                 .then(data => console.log(data))
-            
-         } catch (error) {
-            alert('error!!')
-         }
-         
-    }
-    
 
-    return(
-        <>  
-            <form 
+        } catch (error) {
+            alert('error!!')
+        }
+
+    }
+
+
+    return (
+        <>
+            <form
                 className="font-poppins text-cafe flex flex-col mt-6 w-7/12 mx-auto mt-5 sm:6/12 md:w-5/12 lg:w-4/12"
                 onSubmit={submitHandler}
             >
                 <div className="flex justify-between flex-col">
                     <label className="">From:</label>
-                    <input 
-                        type='text' 
+                    <input
+                        type='text'
                         className="rounded"
                         placeholder="zipcode"
                         onChange={(event) => setZipcode1(event.target.value)}
@@ -48,12 +52,39 @@ const Quote = (props) => {
 
                 <div className="flex justify-between flex-col">
                     <label className="">To:</label>
-                    <input 
-                        type='text' 
+                    <input
+                        type='text'
                         className="rounded"
                         placeholder="zipcode"
                         onChange={(event) => setZipcode2(event.target.value)}
                     />
+                </div>
+
+                <div className="flex justify-between flex-col">
+                    <label>Bedrooms</label>
+                    <select
+                        name="Bedrooms"
+                        onChange={(event) => setBedrooms(event.target.value)}
+                    >
+                        <option value='1'>1</option>
+                        <option value='2'>2</option>
+                        <option value='3'>3</option>
+                        <option value='4'>4</option>
+                    </select>
+                </div>
+
+                <div className="flex justify-between flex-col">
+                    <label>Home Type</label>
+                    <select
+                        name="homeType"
+                        onChange={(event) => setHometype(event.target.value)}
+                    >
+                        <option value='House'>House</option>
+                        <option value='Apartment'>Apartment</option>
+                        <option value='Townhouse'>Townhouse</option>
+                        <option value='Mobile Home'>Mobile Home</option>
+                        <option value='Storage Unit'>Storage Unit</option>
+                    </select>
                 </div>
 
                 <div className="flex justify-between flex-col">
